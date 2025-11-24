da6776098191cf24d4358bf6e387919fcc0f0563
Thomas Monk <246525+tmonk@users.noreply.github.com>
2025-11-24 21:01:45 +0000

    chore: bump version to 3 and update python support

--- multe/__init__.py
+++ multe/__init__.py
@@ -8,8 +8,6 @@ either single alternatives or unordered pairs of alternatives.
 from .model import MultichoiceLogit
 from .simulate import simulate_data
 
-
-
 __all__ = [
     "MultichoiceLogit",
     "simulate_data",
